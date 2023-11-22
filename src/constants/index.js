276a55af4e266b041c1eb56702663874ca809d44
ayan471 <pakhiraayan@gmail.com>
2023-11-22 23:46:53 +0530

    built the app

---
+++ src/constants/index.js
@@ -0,0 +1,218 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "MERN Stack Developer Intern",
+    company_name: "Red Gates It Solutions Private Limited",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "February 2023 - May 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and  other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+    ],
+  },
+  {
+    title: "Jr. Web Developer",
+    company_name: "VA Talks Services Private Limited",
+    icon: tesla,
+    iconBg: "#fbc3bc",
+    date: "May 2023 - October 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "CV Dragon",
+    icon: meta,
+    iconBg: "#a2d2ff",
+    date: "October 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/ayan471",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/ayan-pakhira-93a18baa/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Quill",
+    description:
+      "Developed a web application that uploads PDF and reads the content that the user asked",
+    link: "https://github.com/ayan471/Quill",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Full Stack Spotify Clone",
+    description:
+      'Created a full-stack replica of the popular music platform "Spotify," enabling users to create their own mmusic play list and upload their own choice of songs.',
+    link: "https://github.com/ayan471/spotify-clone",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Airbnb",
+    description:
+      "Designed and built a Airbnb clone for users to book their hotels of their own choice and can also list their property.",
+    link: "https://github.com/ayan471/airbnb2",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Movix",
+    description:
+      "Built a complete clone of IMDB, allowing users to search movies of their choice.",
+    link: "https://github.com/ayan471/Movix",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Jotion",
+    description: "Developed a web application for sharing notes and documents.",
+    link: "https://github.com/ayan471/Jotion",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "MERN Stack Ecommerce Store",
+    description:
+      "An Ecommerce store where users can buy products and sellesrs can list also with admin dashboard and user dashboard",
+    link: "https://github.com/ayan471/mernEcommerce",
+  },
+];
